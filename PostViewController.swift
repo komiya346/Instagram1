03abe30ac127d54e91530fa415bc86f51df2c663
komiya346 <1mg9720s@komazawa-u.ac.jp>
2022-07-08 21:15:23 +0900

    7/8

--- PostViewController.swift
+++ PostViewController.swift
@@ -5,4 +5,14 @@
 //  Created by 越智三四郎 on 2022/07/01.
 //
 
-import Foundation
+import UIKit
+import Firebase
+
+class PostViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+    
+    
--- TabBarController.swift
+++ TabBarController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class TabBarController: UITabBarController, UITabBarControllerDelegate {
 
@@ -14,7 +15,7 @@ class TabBarController: UITabBarController, UITabBarControllerDelegate {
         // タブアイコンの色
        self.tabBar.tintColor = UIColor(red: 1.0, green: 0.44, blue: 0.11, alpha: 1)
        // タブバーの背景色を設定
-       let appearance = UITabBarAppearance()
+    let appearance = UITabBarAppearance()
        appearance.backgroundColor =  UIColor(red: 0.96, green: 0.91, blue: 0.87, alpha: 1)
        self.tabBar.standardAppearance = appearance
        self.tabBar.scrollEdgeAppearance = appearance
@@ -34,16 +35,21 @@ class TabBarController: UITabBarController, UITabBarControllerDelegate {
                    return true
                }
            }
-        
+    override func viewDidAppear(_ animated: Bool) {
+         super.viewDidAppear(animated)
+           
+        // currentUserがnilならログインしていない
+        if Auth.auth().currentUser == nil {
+           // ログインしていないときの処理
+           let loginViewController = self.storyboard?.instantiateViewController(withIdentifier: "Login")
+           self.present(loginViewController!, animated: true, completion: nil)
+       }
     }
+}
     
-
+    
     /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     // Pass the selected object to the new view controller*/
